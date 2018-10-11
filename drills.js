95a130523c10cb701a346f42dc69b20810efe37b
john <johnjacobcosgrove@gmail.com>
2018-10-11 15:33:55 -0400

    'finished' bucketsort

--- drills.js
+++ drills.js
@@ -1,8 +1,12 @@
 'use strict';
 
 let data = [
-  89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40, 48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93, 98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91, 9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38, 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5
-];
+  89, 30, 25, 32, 72, 70, 51, 42, 25, 24, 53, 55, 78, 50, 13, 40,
+  48, 32, 26, 2, 14, 33, 45, 72, 56, 44, 21, 88, 27, 68, 15, 62, 93,
+  98, 73, 28, 16, 46, 87, 28, 65, 38, 67, 16, 85, 63, 23, 69, 64, 91,
+  9, 70, 81, 27, 97, 82, 6, 88, 3, 7, 46, 13, 11, 64, 76, 31, 26, 38,
+  28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87,
+  49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5];
 
 function swap(array, i, j) {
   const tmp = array[i];
@@ -77,45 +81,35 @@ function mergeSort(arr) {
 //console.log(mergeSort(data));
 
 function bucketSort(arr,lowest,highest){
-  let lower =[]
-  let higher = []
-  let newArr = [] 
-  let middle = Math.floor(highest-lowest)
+  const tempArray =[]
   
-  for(let i = 0;i <arr.length; i++){
-    if(arr[i]>middle){
-      higher.push(arr[i])
-    }else{
-      lower.push(arr[i])
+  for(let i = lowest;i <=highest; i++){
+    if(arr.indexOf(i)>0){
+      tempArray.push(arr[arr.indexOf(i)])
     }
   };
-    newArr.push(...lower,...higher)
-    return newArr 
+  return tempArray
 };
 
-const data2 = [6,4,2,1,8,9,7,10,6,5,3]
+//using lowest and highest
+//temp storage is allowed ///an array dur
+//thinking in cards sorting by rank
+//knowing they are integers is a big hint ///able to use > or <
+//two for loops
 
-//console.log(bucketSort(data2,1,10))
+
+const data2 = [6,4,2,1,8,9,7,10,6,5,3,89]
+
+console.log(bucketSort(data2,1,89))
 
 
 function randomSort(arr){
   for(let i = 0;i<arr.length;i++){
-    let i = Math.floor(Math.random()*arr.length)
+    let k = Math.floor(Math.random()*arr.length)
     let j =Math.floor(Math.random()*arr.length)
-    swap(arr,i,j)
+    swap(arr,k,j)
   }
   return arr
 }
 
 // console.log(randomSort(data2))
-
-
-
-// aba
-//akaer
-//aaaaa
-//bee
-
-//aba akaer aaaa
-//aa ///ba //kaer 
-//bee
